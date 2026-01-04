121d48e5ddc89bfb32e24ce44e801240ac9ae030
zakariyaehaourir <zikohaourir@gmail.com>
2026-01-04 22:57:27 +0100

    Added  reader object to read data and associate each email full content to a label

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-from src.core.dataset import Reader
+from src.core.dataset.Reader import Reader
+from src.config.settings import HAM_FOLDER,SPAM_FOLDER
+if __name__ == "__main__": 
+    reader = Reader()
+    X,Y =reader.build_dataset_raw(HAM_FOLDER,SPAM_FOLDER)
+    print(X[0])
+    print(Y[0])
--- src/config/settings.py
+++ src/config/settings.py
@@ -1,4 +1,9 @@
-SPAM_FOLDER = "src/data/raw/spam"
-HAM_FOLDER= "src/data/raw/easy_ham"
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+RAW_DATA_PATH = os.path.join(BASE_DIR, "data", "raw")
+SPAM_FOLDER = os.path.join(RAW_DATA_PATH, "spam")
+HAM_FOLDER = os.path.join(RAW_DATA_PATH, "easy_ham")
+
+PROCESSED_DATA_PATH = os.path.join(BASE_DIR, "data", "processed")
 TEST_SIZE=0.2
