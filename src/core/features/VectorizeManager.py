995a79c886c4e9219a6ad490b5479b2366f6c71d
zakariyaehaourir <zikohaourir@gmail.com>
2026-01-07 22:51:18 +0100

    Adds modular vectorization framework for text features

---
+++ src/core/features/VectorizeManager.py
@@ -0,0 +1,13 @@
+from .Vectorizer import Vectorizer
+class VectorizeManager:
+    def __init__(self, vect:Vectorizer):
+        self.vectorizer = vect
+
+    def transform(self,X):
+        return self.vectorizer.transorm(X)
+    
+    def build_vocabulary(self,X):
+        self.vectorizer.set_vocabulary(X)
+        
+    def get_vocabulary(self):
+        return self.vectorizer.get_vocabulary()
